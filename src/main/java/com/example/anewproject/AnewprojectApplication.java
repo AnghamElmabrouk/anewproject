ba5a0a210b0baadf9f474a0a9226ebc71949770c
Angham MABROUK <anghamelmabrouk1@gmail.com>
2023-12-12 09:13:33 +0100

    project initialization

---
+++ src/main/java/com/example/anewproject/AnewprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.anewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnewprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnewprojectApplication.class, args);
+    }
+
+}
